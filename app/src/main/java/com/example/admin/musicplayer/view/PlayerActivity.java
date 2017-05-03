f5cdb5deab4834ceed11dd98d4f423ea2edfb6e0
AnhTuanKool <leanhtuan.it14@gmail.com>
2017-05-03 23:47:42 +0700

    change flie PlayerActivity.java

--- app/src/main/java/com/example/admin/musicplayer/view/PlayerActivity.java
+++ app/src/main/java/com/example/admin/musicplayer/view/PlayerActivity.java
@@ -25,19 +25,19 @@ public class PlayerActivity extends AppCompatActivity implements View.OnClickLis
     public static ArrayList<SongModel> arrSongs = new ArrayList<>();
     private int seekForwardTime = 5000; // 5000 milliseconds
     private int seekBackwardTime=5000; // 5000 milliseconds
-    private int currentSongIndex = 0;
+    private int songIndex = 0;
     // Media Player
-    private  MediaPlayer mediaPlayer;
+    private  MediaPlayer mp;
     //Handler to update UI timer, progress bar etc....
     private Handler mHandler = new Handler();
-    private ImageView playButton;
-    private ImageView forwardButton;
-    private ImageView backwardButton;
-    private ImageView nextButton;
-    private ImageView previousButton;
+    private ImageView btnPlay;
+    private ImageView btnForward;
+    private ImageView btnBackward;
+    private ImageView btnNext;
+    private ImageView btnPrevious;
     private SeekBar songProgressBar;
-    private TextView currentDurationTextView;
-    private TextView totalDurationTextView;
+    private TextView lblCurrentDuration;
+    private TextView lblTotalDuration;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,40 +55,125 @@ public class PlayerActivity extends AppCompatActivity implements View.OnClickLis
         arrSongs = songManager.getPlayList();
 
         //Media player is used to play music
-        mediaPlayer = new MediaPlayer();
+        mp = new MediaPlayer();
 
         // map views to player_activity_xml
-        playButton=(ImageView) findViewById(R.id.btn_play);
-        nextButton=(ImageView) findViewById(R.id.btn_next_song);
-        previousButton=(ImageView) findViewById(R.id.btn_back_song);
-        forwardButton=(ImageView) findViewById(R.id.btn_forward);
-        backwardButton=(ImageView) findViewById(R.id.btn_rewind);
+        btnPlay=(ImageView) findViewById(R.id.btn_play);
+        btnNext=(ImageView) findViewById(R.id.btn_next_song);
+        btnPrevious=(ImageView) findViewById(R.id.btn_back_song);
+        btnForward=(ImageView) findViewById(R.id.btn_forward);
+        btnBackward=(ImageView) findViewById(R.id.btn_rewind);
         songProgressBar=(SeekBar) findViewById(R.id.progress_seekbar);
-        currentDurationTextView=(TextView) findViewById(R.id.tv_current_time);
-        totalDurationTextView=(TextView) findViewById(R.id.tv_total_time);
+        lblCurrentDuration=(TextView) findViewById(R.id.tv_current_time);
+        lblTotalDuration=(TextView) findViewById(R.id.tv_total_time);
         // call event listener of each view
-        playButton.setOnClickListener(this);
-        forwardButton.setOnClickListener(this);
-        backwardButton.setOnClickListener(this);
-        nextButton.setOnClickListener(this);
-        previousButton.setOnClickListener(this);
+        btnPlay.setOnClickListener(this);
+        btnForward.setOnClickListener(this);
+        btnBackward.setOnClickListener(this);
+        btnNext.setOnClickListener(this);
+        btnPrevious.setOnClickListener(this);
         
         songProgressBar.setOnSeekBarChangeListener(this);// bat su kien de user biet bai hat dang tua den dau
-        mediaPlayer.setOnCompletionListener(this);// su kien de biet khi nao mediaplayer choi xong 1 bai nhac
+        mp.setOnCompletionListener(this);// su kien de biet khi   nao mediaplayer choi xong 1 bai nhac
         
-        playSong(currentSongIndex);
+        playSong(songIndex);
 
 
-    }
-
-    private void playSong(int currentSongIndex) {
     }
 
     @Override
-    public void onCompletion(MediaPlayer mp) {
+    public boolean onCreateOptionsMenu(Menu menu){
+         MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.header_menu, menu);
+        return true;
+    }
+    private void playSong(int songIndex) {
+        //Play song
+        try {
+            mp.reset();
+            mp.setDataSource(arrSongs.get(songIndex).path);
+            mp.prepare();
+            mp.start();
 
+            //Update title of toolbar
+            toolBar.setTitle(arrSongs.get(songIndex).title);
+
+            //Changing Button Image to pause image
+            btnPlay.setImageResource(R.drawable.pause);
+
+            //set Progress bar values
+            songProgressBar.setProgress(0);
+            songProgressBar.setMax(100);
+
+            //Updating progress bar
+            updateProgressBar();
+
+            //notification
+            buildNotification();
+
+        } catch (IllegalArgumentException e){
+            e.pintStackTrace();
+        }catch (IllegalStateException e){
+            e.pintStackTrace();
+        }catch (IOException e){
+            e.pintStackTrace();
+        }
     }
 
+    /**
+     *Update timer on seekbar
+     */
+    public void updateProgressBar(){
+        mHandler.postDelayed(mUpdateTimeTask, 100);
+    }
+
+    /**
+     * Background Runnable thread
+     */
+    private  Runnable mUpdateTimeTask = () -> {
+        public void run(){
+            try {
+                long totalDuration = mp.getDuration();
+                long curentDuảtion = mp.getCurentPosition();
+
+                //Displaying Total Duration time
+                lblTotalDuration.setText(""+Util.milliSecondsToTimer(totalDuration));
+                //Displaying time complete playing
+                lblCurrentDuration.setText(""+Util.milliSecondsToTimer(currentDuration));
+
+                //Updating progress bar
+                int progress = (int)(Util.getProgressPercentage(currentDuration, totalDuration));
+                songProgressBar.setProgress(progress);
+                //Running this thread after 100 millisecond
+                mHandler.postDelayed(this, 100);
+            }catch (Exception ex){
+
+            }
+        }
+    };
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        if(songIndex < (arrSongs.size() - 1)){
+            playSong(songIndex + 1);
+            songIndex = songIndex + 1;
+        }else{
+            //play first song
+            playSong(0);
+            songIndex = 0;
+        }
+        buildNotification();
+    }
+    private  void buildNotification(){
+        Intent intent = new Intent(getApplicationContext(),PlayerActivity.class);
+        PendingIntent pendingIntent = PendingItent.getService(getApplicationContext() , 1 ,ontent 0)
+        Notification.Builder builder = new Notification.Builder(this)
+                .setSmallIcon(R.drawable.song)
+                .setContentTitle("Media Artist")
+                .setContentText(arrSongs.get(songIndex).title)
+                .setDeleteIntent(pendingIntent);
+        NotificationManager notificationManager = (NotificationManager) ()
+    }
     @Override
     public void onClick(View v) {
 
